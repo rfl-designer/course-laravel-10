768d7fb9fd588e2682f81bd9ed58f8fe9ca49800
rfl-designer <rfldesigner@outlook.com>
2023-04-04 11:26:24 -0300

    feat: add contact view, controller and rout

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::get('/contato', [SiteController::class, 'contact']);
 
 Route::get('/', function () {
     return view('welcome');
