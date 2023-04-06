de595483dfca15485e538cd5df2d36322ddfd9e7
rfl-designer <rfldesigner@outlook.com>
2023-04-06 12:55:50 -0300

    feat: add form create/update in component

--- resources/views/admin/supports/create.blade.php
+++ resources/views/admin/supports/create.blade.php
@@ -7,8 +7,5 @@
 @endif
 
 <form action="{{ route('supports.store') }}" method="POST">
-    @csrf
-    <input type="text" placeholder="Assunto" name="subject" value="{{ old('subject') }}">
-    <textarea name="body" id="" cols="30" rows="5" placeholder="Descrição">{{ old('body') }}</textarea>
-    <button type="submit">Enviar</button>
+    @include('admin.supports.partials.form')
 </form>
--- resources/views/admin/supports/edit.blade.php
+++ resources/views/admin/supports/edit.blade.php
@@ -7,9 +7,8 @@
 </ul>
 
 <form action="{{ route('supports.update', $support->id) }}" method="POST">
-    @csrf()
     @method('put')
-    <input type="text" placeholder="Assunto" name="subject" value="{{ $support->subject }}">
-    <textarea name="body" id="" cols="30" rows="5" placeholder="Descrição">{{ $support->body }}</textarea>
-    <button type="submit">Enviar</button>
+    @include('admin.supports.partials.form', [
+        'support' => $support
+    ])
 </form>
