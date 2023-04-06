c218bbc300623b712bb72299c41c75151b5f1241
rfl-designer <rfldesigner@outlook.com>
2023-04-06 12:13:48 -0300

    feat: implement inverse dependencies

---
+++ app/Repositories/SupportEloquentORM.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\DTO\{
+    CreateSupportDTO,
+    UpdateSupportDTO
+};
+use App\Repositories\SupportRepositoryInterface;
+
+class SupportEloquentORM implements SupportRepositoryInterface
+{
+    public function getAll(string $filter = null):array
+    {
+        
+    }
+
+    public function findOne(string $id):stdClass|null
+    {
+
+    }
+
+    public function delete(string $id): void
+    {
+
+    }
+
+    public function new(CreateSupportDTO $dto): stdClass
+    {
+
+    }
+
+    public function update(UpdateSupportDTO $dto): stdClass|null
+    {
+
+    }
+
+}
